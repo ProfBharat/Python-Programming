c074798c7d4822bc5303d4cbc437f42f7cfe15f3
Bharat Kumar <bharatagape@gmail.com>
2022-12-06 13:12:28 +0530

    changing object name to fruits1,2

--- 27_class.py
+++ 27_class.py
@@ -1,17 +1,19 @@
-
+#creating class and its objects in python  
 class fruit:
     
     def __init__(self,name, cost):
         self.name = name
         self.cost = cost
 
-    def greet(self):
+    def greet(self):    #greet() is function
         #print("hai")
-        return f"hey Are you Looking {self.name}?"
-mango = fruit("Mango",30)
-papaya = fruit("Papaya",50)
+        return f"hey, Are you Looking {self.name} its cost is {self.cost}?"
+        
+fruit1 = fruit("Mango",30)       #mango is an object
+fruit2 = fruit("Papaya",50)     #papaya is an object
 
-print(mango.greet())
-print(mango.cost)
-print(mango.name)
-print(f"{papaya.name} its cost is {papaya.cost}")
+print(fruit1.greet())
+
+#print(mango.cost)
+#print(mango.name)
+#print(f"{papaya.name} its cost is {papaya.cost}")
